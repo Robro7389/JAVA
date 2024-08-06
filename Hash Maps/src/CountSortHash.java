77d63e16a78bab8f2f2f6584e61681a6c8b210ba
Ronak Gour <ronakgour7389@gmail.com>
2024-08-06 12:01:00 +0530

    Count Sort using hash map

---
+++ Hash Maps/src/CountSortHash.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class CountSortHash {
+    public static void main(String[] args) {
+        int[] arr = {3,4,1,3,2,5,2,8};
+        countSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void countSort(int[] arr){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int max = Arrays.stream(arr).max().getAsInt();
+        int min = Arrays.stream(arr).min().getAsInt();
+
+        for (int num : arr){
+            map.put(num,map.getOrDefault(num,0) + 1);
+        }
+
+        int index = 0;
+        for (int i = min; i <= max ; i++) {
+            int count = map.getOrDefault(i,0);
+            for (int j = 0; j < count; j++) {
+                arr[index] = i;
+                index++;
+            }
+        }
+    }
+}
